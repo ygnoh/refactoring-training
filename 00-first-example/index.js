04ac357dba649c10f9198cb96c6ec50c5eef5d75
yonggoo-noh <yonggoo.noh@navercorp.com>
2020-11-05 21:57:18 +0900

    Split loop

--- 00-first-example/index.js
+++ 00-first-example/index.js
@@ -36,6 +36,9 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    }
+
+    for (let perf of invoice.performances) {
         totalAmount += amountFor(perf);
     }
 
