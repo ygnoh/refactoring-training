896917c0a81ca7d1917ca66fefa9f7f4bb9af325
yonggoo-noh <yonggoo.noh@navercorp.com>
2020-11-05 21:57:53 +0900

    Slide statements

--- 00-first-example/index.js
+++ 00-first-example/index.js
@@ -31,12 +31,13 @@ const invoices = {
 };
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `> result (customer: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
+
+    let totalAmount = 0;
 
     for (let perf of invoice.performances) {
         totalAmount += amountFor(perf);
