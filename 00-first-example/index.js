476f2e9c8185b642839eacb56f6a01c9a3c71c78
yonggoo-noh <yonggoo.noh@navercorp.com>
2020-11-05 21:35:38 +0900

    Inline variable

--- 00-first-example/index.js
+++ 00-first-example/index.js
@@ -40,16 +40,15 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(perf, play);
+        let thisAmount = amountFor(perf, playFor(perf));
 
         volumeCredits += Math.max(perf.audience - 30, 0);
 
-        if ("comedy" === play.type) {
+        if ("comedy" === playFor(perf).type) {
             volumeCredits += Math.floor(perf.audience / 5);
         }
 
-        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
 
